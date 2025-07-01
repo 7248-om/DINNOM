e8441cc0dc40d34fb648e0e7264c40e5e1475473
Nihar Mehta <nkmehta2162@gmail.com>
2025-07-01 17:06:22 +0530

    final all working

---
+++ DINNOM/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="w-64 h-32 bg-blue-500 text-white flex items-center justify-center rounded-lg shadow-lg m-8">
+              Tailwind is working!
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
