48a097c3e2f817711f6eca573a718929901b6470
Diya Satish <diyamenon444@gmail.com>
2025-07-02 15:31:37 +0400

    footer main

--- DINNOM/src/App.jsx
+++ DINNOM/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './styles/App.css'
 import Navbar from './components/navbar'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
       </Routes>
+      <Footer /> 
     </BrowserRouter>
   )
 }
