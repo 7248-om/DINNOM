1f6d32a4d400afc3c3a24f15fb4e942f47edcdf3
Nihar Mehta <nkmehta2162@gmail.com>
2025-07-02 16:58:52 +0530

    backend ready

--- DINNOM/src/App.jsx
+++ DINNOM/src/App.jsx
@@ -2,12 +2,26 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './styles/App.css'
 import Navbar from './components/navbar'
 import Home from './pages/Home'
+import React, { useEffect, useState } from 'react'
+
 function App() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+  fetch('http://localhost:5000/')
+    .then((res) => res.text())
+    .then((data) => {
+      console.log('Winners:', data);
+      setMessage(data);
+    });
+}, []);
+
   return (
     <BrowserRouter>
       <Navbar />
+      <div className="text-center text-blue-700 font-bold mt-4">{message}</div>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
