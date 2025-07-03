8a4b2f4f277e7715804e9597ae3cba4c97991312
Nihar Mehta <nkmehta2162@gmail.com>
2025-07-03 16:59:58 +0530

    backend connected with mongo

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 5050;
+
+const MONGO_URI = 'mongodb://localhost:27017'; 
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('MongoDB connected!'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(cors());
 app.use(express.json());
