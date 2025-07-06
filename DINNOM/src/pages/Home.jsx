c2ebe1eaa98efbbcbd54116f789f3f8c929a6dcd
Om Joshi <joshiom564@gmail.com>
2025-07-06 16:51:14 +0530

    (unfinished) grid section in homepage

--- DINNOM/src/pages/Home.jsx
+++ DINNOM/src/pages/Home.jsx
@@ -3,12 +3,13 @@ import HeroSlider from '../components/HeroSlider';
 import GenerationSection from '../components/GenerationSection';
 import SummerSection from '../components/SummerSection';
 import FabricSection from '../components/OurFabric';
-
+import GridMoodBoard from '../components/GridMoodBoard';
 const Home = () => {
   return (
     <div className="w-full h-full">
       <HeroSlider />
       <FabricSection />
+      <GridMoodBoard />
       <GenerationSection />
       <SummerSection />
     </div>
