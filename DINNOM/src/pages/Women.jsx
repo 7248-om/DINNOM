c361b74f64e7cf56d1533d0aafb753daee97acc1
Diya Satish <diyamenon444@gmail.com>
2025-07-12 01:21:55 +0400

    name changes to file

--- DINNOM/src/pages/Women.jsx
+++ DINNOM/src/pages/Women.jsx
@@ -1,28 +1,31 @@
 // import React from "react";
-// import Navbar from "../components/navbar"; // Adjust the path as needed
+// import Navbar from "../components/navbar"; // Adjust path if needed
 
 // import IntroTransition from '../components/IntroTransition';
 // import WomenCategoryShowcase1 from "../components/women/WomenCategoryShowcase1";
-// // import WomenCategoryShowcase2 from "../components/women/WomenCategoryShowcase2";
-// // import WomenCategoryShowcase3 from "../components/women/WomenCategoryShowcase3";
-// // import WomenCategoryShowcase4 from "../components/women/WomenCategoryShowcase4";
-// // import WomenCategoryShowcase5 from "../components/women/WomenCategoryShowcase5";
-// import img2 from '../assets/Women/images/img1.png'; // Make sure this image exists
+// import WomenCategoryShowcase2 from "../components/women/WomenCategoryShowcase2";
+// import WomenCategoryShowcase3 from "../components/women/WomenCategoryShowcase3";
+// import WomenCategoryShowcase4 from "../components/women/WomenCategoryShowcase4";
+// import WomenCategoryShowcase5 from "../components/women/WomenCategoryShowcase5";
+// import WomenCategoryShowcase6 from "../components/women/WomenCategoryShowcase6";
+// import img2 from '../assets/Women/images/img1.png'; // Make sure this exists
 
 // const Women = () => {
 //   return (
 //     <div className="w-full">
 //       <IntroTransition image={img2} text="WOMEN'S COLLECTION" />
 //       <WomenCategoryShowcase1 />
-//       {/* <WomenCategoryShowcase2 />
+//       <WomenCategoryShowcase2 />
 //       <WomenCategoryShowcase3 />
 //       <WomenCategoryShowcase4 />
-//       <WomenCategoryShowcase5 /> */}
+//       <WomenCategoryShowcase5 />
+//       <WomenCategoryShowcase6 />
 //     </div>
 //   );
 // };
 
 // export default Women;
+
 
 import React from 'react'
 import Navbar from '../components/navbar'
