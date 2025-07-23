2f84b85ab1414dbc3d3bb1a608c00ba3529bdad0
Om Joshi <joshiom564@gmail.com>
2025-07-23 13:15:14 +0530

    profile cover page

--- DINNOM/src/App.jsx
+++ DINNOM/src/App.jsx
@@ -47,6 +47,10 @@ import AdminStats from './pages/admin/AdminStats';
 import AdminOrders from './pages/admin/AdminOrders';
 import AdminProtectedRoute from './components/AdminProtectedRoute';
 
+//Profile Pages
+import Profile from './pages/Profile/Profile';
+
+
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const location = useLocation();
@@ -80,6 +84,7 @@ function App() {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/order-success/:orderId" element={<OrderSuccess />} />
           <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/profile" element={<Profile />} />
 
           {/* Admin Routes */}
           <Route element={<AdminProtectedRoute />}>
